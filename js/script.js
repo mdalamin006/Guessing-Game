5174fa8e7cfdde9c7c5cc22a4b620a78b7ef9716
Md. Al-Amin <145653719+mdalamin006@users.noreply.github.com>
2023-12-11 15:59:28 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let numberOfLost=0;
 for (let x=1;x<=5; x++)
 {
 let guessingNum=parseInt(prompt("Enter The Number From 1 to 5: "));
-let numGuessing=Math.floor(Math.random()*6);
+let numGuessing=Math.floor(Math.random()*5)+1;
 if (guessingNum==numGuessing){
     document.write("<h4> You Have Won. The Number Is ="+numGuessing+"<br></h4>");
     numberOfWon++
